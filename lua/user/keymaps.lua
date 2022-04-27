072ff738b50078760fc6afef8a171e82bda8c54b
Carl Chen <ccf.developer@gmail.com>
2022-04-27 11:09:54 +0800

    feat: add packer and nvim-tree

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,23 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", ";", "<Nop>", opts)
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ";"
+
+-- Modes normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t", command_mode = "c",
+--
+
+-- FileExpoler
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+keymap("n", "<leader>f", ":NvimTreeFindFile<cr>", opts)
+
