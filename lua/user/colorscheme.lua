f80c60fa054da84b5bae74141e469a6875c2eb87
Carl Chen <ccf.developer@gmail.com>
2022-07-06 19:26:25 +0800

    feat: make theme config work

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+require "user.themes.github-nvim-theme"
+
+local colorscheme = "github_dark"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
