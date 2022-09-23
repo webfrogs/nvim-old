93271049c4df5cd0b11c8d30754253a2757fbddc
Carl Chen <ccf.developer@gmail.com>
2022-09-23 22:32:43 +0800

    fix: nvim tree config issue

--- lua/user/conf/nvim-tree.lua
+++ lua/user/conf/nvim-tree.lua
@@ -65,7 +65,7 @@ nvim_tree.setup({
   update_cwd = false,
   view = {
     width = 30,
-    height = 30,
+    --height = 30,
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
