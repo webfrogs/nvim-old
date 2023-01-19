c970b26c483646c2c240f286cac8dc90d228715c
Carl Chen <ccf.developer@gmail.com>
2023-01-19 12:56:51 +0800

    feat: add new plugin: hop

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -77,6 +77,14 @@ return packer.startup(function(use)
       'kyazdani42/nvim-web-devicons', -- optional, for file icon
     }
   }
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
