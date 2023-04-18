f522c8af3b7da9b051877836bb44d956417bd5cc
Carl Chen <ccf.developer@gmail.com>
2023-04-18 14:33:46 +0800

    fix: nvim tree keymap setting change

--- lua/user/conf/nvim-tree.lua
+++ lua/user/conf/nvim-tree.lua
@@ -1,43 +1,4 @@
 -- https://github.com/kyazdani43/nvim-tree.lua
---
--- <CR> or o on the root folder will cd in the above directory
--- <C-]> will cd in the directory under the cursor
--- <BS> will close current opened directory or parent
--- type a to add a file. Adding a directory requires leaving a leading / at the end of the path.
--- you can add multiple directories by doing foo/bar/baz/f and it will add foo bar and baz directories and f as a file
---
--- type r to rename a file
--- type <C-r> to rename a file and omit the filename on input
--- type x to add/remove file/directory to cut clipboard
--- type c to add/remove file/directory to copy clipboard
--- type y will copy name to system clipboard
--- type Y will copy relative path to system clipboard
--- type gy will copy absolute path to system clipboard
--- type p to paste from clipboard. Cut clipboard has precedence over copy (will prompt for confirmation)
--- type d to delete a file (will prompt for confirmation)
--- type D to trash a file (configured in setup())
--- type ]c to go to next git item
--- type [c to go to prev git item
--- type - to navigate up to the parent directory of the current file/directory
--- type s to open a file with default system application or a folder with default file manager (if you want to change the command used to do it see :h nvim-tree.setup under system_open)
--- if the file is a directory, <CR> will open the directory otherwise it will open the file in the buffer near the tree
--- if the file is a symlink, <CR> will follow the symlink (if the target is a file)
--- <C-v> will open the file in a vertical split
--- <C-x> will open the file in a horizontal split
--- <C-t> will open the file in a new tab
--- <Tab> will open the file as a preview (keeps the cursor in the tree)
--- I will toggle visibility of hidden folders / files
--- H will toggle visibility of dotfiles (files/folders starting with a .)
--- R will refresh the tree
--- Double left click acts like <CR>
--- Double right click acts like <C-]>
--- W will collapse the whole tree
--- S will prompt the user to enter a path and then expands the tree to match the path
--- . will enter vim command mode with the file the cursor is on
--- C-k will toggle a popup with file infos about the file under the cursor
---
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
@@ -45,11 +6,68 @@ if not status_ok then
   return
 end
 
--- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
--- if not config_status_ok then
---   return
--- end
--- local tree_cb = nvim_tree_config.nvim_tree_callback
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- keymaps
+  vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node,          opts('CD'))
+  vim.keymap.set('n', '<C-e>', api.node.open.replace_tree_buffer,     opts('Open: In Place'))
+  vim.keymap.set('n', '<C-k>', api.node.show_info_popup,              opts('Info'))
+  vim.keymap.set('n', '<C-r>', api.fs.rename_sub,                     opts('Rename: Omit Filename'))
+  vim.keymap.set('n', 't',     api.node.open.tab,                     opts('Open: New Tab'))
+  vim.keymap.set('n', '<C-v>', api.node.open.vertical,                opts('Open: Vertical Split'))
+  vim.keymap.set('n', '<C-x>', api.node.open.horizontal,              opts('Open: Horizontal Split'))
+  vim.keymap.set('n', '<BS>',  api.node.navigate.parent_close,        opts('Close Directory'))
+  vim.keymap.set('n', '<CR>',  api.node.open.edit,                    opts('Open'))
+  vim.keymap.set('n', '<Tab>', api.node.open.preview,                 opts('Open Preview'))
+  vim.keymap.set('n', '>',     api.node.navigate.sibling.next,        opts('Next Sibling'))
+  vim.keymap.set('n', '<',     api.node.navigate.sibling.prev,        opts('Previous Sibling'))
+  vim.keymap.set('n', '.',     api.node.run.cmd,                      opts('Run Command'))
+  vim.keymap.set('n', '-',     api.tree.change_root_to_parent,        opts('Up'))
+  vim.keymap.set('n', 'a',     api.fs.create,                         opts('Create'))
+  vim.keymap.set('n', 'bmv',   api.marks.bulk.move,                   opts('Move Bookmarked'))
+  vim.keymap.set('n', 'B',     api.tree.toggle_no_buffer_filter,      opts('Toggle No Buffer'))
+  vim.keymap.set('n', 'c',     api.fs.copy.node,                      opts('Copy'))
+  vim.keymap.set('n', 'C',     api.tree.toggle_git_clean_filter,      opts('Toggle Git Clean'))
+  vim.keymap.set('n', '[c',    api.node.navigate.git.prev,            opts('Prev Git'))
+  vim.keymap.set('n', ']c',    api.node.navigate.git.next,            opts('Next Git'))
+  vim.keymap.set('n', 'd',     api.fs.remove,                         opts('Delete'))
+  vim.keymap.set('n', 'D',     api.fs.trash,                          opts('Trash'))
+  vim.keymap.set('n', 'E',     api.tree.expand_all,                   opts('Expand All'))
+  vim.keymap.set('n', 'e',     api.fs.rename_basename,                opts('Rename: Basename'))
+  vim.keymap.set('n', ']e',    api.node.navigate.diagnostics.next,    opts('Next Diagnostic'))
+  vim.keymap.set('n', '[e',    api.node.navigate.diagnostics.prev,    opts('Prev Diagnostic'))
+  vim.keymap.set('n', 'F',     api.live_filter.clear,                 opts('Clean Filter'))
+  vim.keymap.set('n', 'f',     api.live_filter.start,                 opts('Filter'))
+  vim.keymap.set('n', 'g?',    api.tree.toggle_help,                  opts('Help'))
+  vim.keymap.set('n', 'gy',    api.fs.copy.absolute_path,             opts('Copy Absolute Path'))
+  vim.keymap.set('n', 'H',     api.tree.toggle_hidden_filter,         opts('Toggle Dotfiles'))
+  vim.keymap.set('n', 'I',     api.tree.toggle_gitignore_filter,      opts('Toggle Git Ignore'))
+  vim.keymap.set('n', 'J',     api.node.navigate.sibling.last,        opts('Last Sibling'))
+  vim.keymap.set('n', 'K',     api.node.navigate.sibling.first,       opts('First Sibling'))
+  vim.keymap.set('n', 'm',     api.marks.toggle,                      opts('Toggle Bookmark'))
+  vim.keymap.set('n', 'o',     api.node.open.edit,                    opts('Open'))
+  vim.keymap.set('n', 'O',     api.node.open.no_window_picker,        opts('Open: No Window Picker'))
+  vim.keymap.set('n', 'p',     api.fs.paste,                          opts('Paste'))
+  vim.keymap.set('n', 'P',     api.node.navigate.parent,              opts('Parent Directory'))
+  vim.keymap.set('n', 'q',     api.tree.close,                        opts('Close'))
+  vim.keymap.set('n', 'r',     api.fs.rename,                         opts('Rename'))
+  vim.keymap.set('n', 'R',     api.tree.reload,                       opts('Refresh'))
+  vim.keymap.set('n', 's',     api.node.run.system,                   opts('Run System'))
+  vim.keymap.set('n', 'S',     api.tree.search_node,                  opts('Search'))
+  vim.keymap.set('n', 'U',     api.tree.toggle_custom_filter,         opts('Toggle Hidden'))
+  vim.keymap.set('n', 'W',     api.tree.collapse_all,                 opts('Collapse'))
+  vim.keymap.set('n', 'x',     api.fs.cut,                            opts('Cut'))
+  vim.keymap.set('n', 'y',     api.fs.copy.filename,                  opts('Copy Name'))
+  vim.keymap.set('n', 'Y',     api.fs.copy.relative_path,             opts('Copy Relative Path'))
+  vim.keymap.set('n', '<2-LeftMouse>',  api.node.open.edit,           opts('Open'))
+  vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
+
+end
 
 nvim_tree.setup({
   auto_reload_on_write = true,
@@ -60,6 +78,7 @@ nvim_tree.setup({
   open_on_tab = false,
   sort_by = "name",
   update_cwd = false,
+  on_attach = on_attach,
   view = {
     width = 30,
     --height = 30,
@@ -69,14 +88,6 @@ nvim_tree.setup({
     number = false,
     relativenumber = false,
     signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- user mappings go here
-        { key = "t",                            action = "tabnew" },
-        { key = "<C-t>",                        action = "" },
-      },
-    },
   },
   renderer = {
     indent_markers = {
