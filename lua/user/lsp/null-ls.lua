c8335a4464275735da309cdaac4ca60cc9b2fbc1
Carl Chen <ccf.developer@gmail.com>
2023-06-22 21:50:17 +0800

    refactor: remove useless config

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,7 @@
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormating", {})
 
+-- used by path 'user/lazy/lsp.lua'
 local opts = {
   sources = {
     null_ls.builtins.formatting.gofmt,
