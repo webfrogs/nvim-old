411d6f3afbc11dbd9e72f2df53bb95aab5da63d5
Carl Chen <ccf.developer@gmail.com>
2023-06-23 11:09:24 +0800

    feat: add gopher plugin

--- lua/user/lazy/golang.lua
+++ lua/user/lazy/golang.lua
@@ -1,3 +1,16 @@
 return {
-  -- "fatih/vim-go",
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function (_, opts)
+      require("gopher").setup(opts)
+    end,
+    keys = {
+      {"<leader>gta", "<cmd>GoTestAdd<cr>", "n", desc="generate one test for current function/method"},
+      {"<leader>gie", "<cmd>GoIfErr<cr>", "n", desc="generate 'if error'"},
+    },
+    -- build = function ()
+    --   vim.cmd [[silent! GoInstallDeps]]
+    -- end,
+  }
 }
